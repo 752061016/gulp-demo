169c773a30e38ae08242a73c0b208860f4c6afe9
unknown <752061016@qq.com>
2020-06-09 21:20:53 +0800

    xxx

---
+++ deploy.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ssh:{
+        remotePath: '/alidata/www/vr-java/public/js/test/gulp-demo',
+        host: '114.55.150.80',
+        port: 22,
+        user: 'root',
+        pass: 'Meijing2018'
+    }
+}
